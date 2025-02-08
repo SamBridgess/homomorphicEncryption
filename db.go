45daa09f3d25d64ccb3f21b8fe728296c420851c
SamBridgess <ilya.minyaeff@yandex.ru>
2025-02-09 02:49:51 +0300

    db functions

---
+++ db.go
@@ -0,0 +1,30 @@
+package homomorphic_encryption_lib
+
+import (
+	"database/sql"
+	"fmt"
+)
+
+type DBConnectionInfo struct {
+	Host     string
+	Port     int
+	User     string
+	Password string
+	DBName   string
+}
+
+func NewDBConnectionInfo(host string, port int, user string, password string, dbname string) *DBConnectionInfo {
+	info := DBConnectionInfo{
+		Host:     host,
+		Port:     port,
+		User:     user,
+		Password: password,
+		DBName:   dbname,
+	}
+	return &info
+}
+
+func OpenConnection(info DBConnectionInfo) (*sql.DB, error) {
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", info.User, info.Port, info.User, info.Password, info.DBName)
+	return sql.Open("postgres", psqlInfo)
+}
