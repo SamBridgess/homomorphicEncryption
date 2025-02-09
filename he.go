e9d3d7512b76264176faa58b667d83589a9f2a69
SamBridgess <ilya.minyaeff@yandex.ru>
2025-02-09 21:30:17 +0300

    HE and keys

---
+++ he.go
@@ -0,0 +1,36 @@
+package homomorphic_encryption_lib
+
+import (
+	"github.com/ldsec/lattigo/v2/ckks"
+	"github.com/ldsec/lattigo/v2/rlwe"
+)
+
+var (
+	ckksParams, _ = ckks.NewParametersFromLiteral(ckks.PN12QP109)
+)
+
+func encryptCKKS(data float64, pk *rlwe.PublicKey) ([]byte, error) {
+	encoder := ckks.NewEncoder(ckksParams)
+	encryptor := ckks.NewEncryptor(ckksParams, pk)
+
+	plaintext := ckks.NewPlaintext(ckksParams, ckksParams.MaxLevel(), ckksParams.DefaultScale())
+	encoder.Encode([]float64{data}, plaintext, ckksParams.LogSlots())
+
+	ciphertext := encryptor.EncryptNew(plaintext)
+	return ciphertext.MarshalBinary()
+}
+
+func decryptCKKS(data []byte, sk *rlwe.SecretKey) (float64, error) {
+	decryptor := ckks.NewDecryptor(ckksParams, sk)
+	ciphertext := ckks.NewCiphertext(ckksParams, 1, ckksParams.MaxLevel(), ckksParams.DefaultScale())
+	err := ciphertext.UnmarshalBinary(data)
+	if err != nil {
+		return 0, err
+	}
+
+	plaintext := decryptor.DecryptNew(ciphertext)
+	encoder := ckks.NewEncoder(ckksParams)
+	decoded := encoder.Decode(plaintext, ckksParams.LogSlots())
+
+	return real(decoded[0]), nil
+}
---
+++ keys.go
@@ -0,0 +1,17 @@
+package homomorphic_encryption_lib
+
+import (
+	"crypto/rand"
+	"github.com/ldsec/lattigo/v2/ckks"
+	"github.com/ldsec/lattigo/v2/rlwe"
+)
+
+func genKeysCKKS(ckksParams ckks.Parameters) (*rlwe.SecretKey, *rlwe.PublicKey) {
+	return ckks.NewKeyGenerator(ckksParams).GenKeyPair()
+}
+
+func genKeyAES() ([]byte, error) {
+	aesKey := make([]byte, 32)
+	_, err := rand.Read(aesKey)
+	return aesKey, err
+}
